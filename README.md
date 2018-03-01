9e82e0c4b0ad882a4ee6284c55852b41e74d8d42
Anthony Ferrando <ferrando.tony@gmail.com>
2018-03-01 09:02:38 +0100

    Update editor API and readme.

--- README.md
+++ README.md
@@ -21,11 +21,17 @@ This repository contains:
 * All source code is contained in the src folder.
 * The src/lib folders contains utilities that are not specific to Hyperstart and could be later exported to their own package.
 * The sources are divided into **modules**, each module lives in its own folder and contains the following sources:
-  * `api.ts`: The type information for the public-facing API of this module
+  * `api.ts`: The type information for the public-facing API of this module, the types for a module are all contained in a namespace.
 * On startup, an `init()` action gets called on each top-level module with the global actions.  
   Each top-level module receives the public facing API of each other module.  
   Each module can store the dependencies it needs from other modules at this point.  
   Note: there is a circular dependency between all the `api.ts` files of all modules, but this causes no issue since these files only contain type information.
+
+# Overall conventions
+
+* All asynchronous actions return a `Promise`, even if not used by the components.
+* All modules are responsible to log their actions with the `logger` module.
+* Important sub-sections of files are marked with `// # Section Name` (same as markdown titles)
 
 # License
 
--- src/editor/api.ts
+++ src/editor/api.ts
@@ -10,24 +10,66 @@ export namespace editor {
   }
 
   export interface SourcesActions {
-    open(sources: string | string[], clearOpened?: boolean): void
-    close(sources: string | string[]): void
-    closeAll(): void
-    select(source: string | null): void
+    open(sources: string | string[], clearOpened?: boolean)
+    close(sources: string | string[])
+    closeAll()
+    select(source: string | null)
+  }
+
+  // # Files
+
+  // ## State
+
+  export interface FilesState {
+    // TODO
+  }
+
+  // ## Actions
+
+  export interface CreateFilePayload {
+    type: "file" | "folder"
+    name: string
+    parent?: projects.File
+  }
+
+  export interface SetContentPayload {
+    path: string
+    content: string
+  }
+
+  export interface FilesActions {
+    toggleExpanded(path: string)
+    create(file: CreateFilePayload): Promise<void>
+    delete(file: string | projects.File): Promise<void>
+    preview(file: string | projects.File)
+    setContent(source: SetContentPayload)
   }
 
   // # State
 
   export interface State {
+    files: FilesState
     localStore: projects.State
     sources: SourcesState
   }
 
   // # Actions
 
+  export interface OpenPayload {
+    project: projects.Project
+    save: boolean
+  }
+
   export interface Actions extends ModuleActions {
+    files: FilesActions
     localStore: projects.Actions
     sources: SourcesActions
-    open(project: projects.Project)
+    open(payload: OpenPayload): Promise<void>
+    close()
+    submitEdits(): Promise<void>
+    setOwner(owner: projects.Owner): Promise<void>
+    saveAllSources(): Promise<void>
+    run(debug: boolean): Promise<void>
+    importProjects(projects: string[]): Promise<void>
   }
 }
