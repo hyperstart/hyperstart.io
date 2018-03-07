99fcfd2f2b46fb6aa6edf7aee081fd6caa65b0c0
Anthony Ferrando <ferrando.tony@gmail.com>
2018-03-07 20:23:52 +0100

    Add Tree component in lib.

---
+++ src/lib/components/Tree.tsx
@@ -0,0 +1,73 @@
+import { h } from "hyperapp"
+
+export interface NodeViewProps<T> {
+  item: T
+  /** Rest of the props, passed from the tree */
+  [name: string]: any
+}
+
+export interface BaseProps<T> {
+  getChildren(item: T): T[] | null
+  toggleExpanded(item: T)
+  View(props: NodeViewProps<T>): any
+  nodesClass?: string
+  nodeClass?: string
+  itemProps?: any
+}
+
+// # Node
+
+interface NodeProps<T> extends BaseProps<T> {
+  item: T
+}
+
+function Node<T>(props: NodeProps<T>) {
+  const { item, ...rest } = props
+  const itemProps = props.itemProps || {}
+  const items = props.getChildren(item)
+  const expanded = items && items.length
+  const children = expanded ? (
+    <NodeList items={items} itemProps={itemProps} {...rest} />
+  ) : null
+  return (
+    <div class={props.nodeClass || ""}>
+      <div
+        onclick={e => {
+          props.toggleExpanded(item)
+        }}
+      >
+        <props.View item={item} {...itemProps} />
+      </div>
+      {children}
+    </div>
+  )
+}
+
+// # NodeList
+
+interface NodeListProps<T> extends BaseProps<T> {
+  items: T[]
+}
+
+function NodeList<T>(props: NodeListProps<T>) {
+  const { items, ...rest } = props
+  const children = items.map(item => <Node item={item} {...rest} />)
+  return <div class={props.nodesClass || ""}>{children}</div>
+}
+
+// # Tree
+
+export interface TreeProps<T> extends BaseProps<T> {
+  items: T[]
+  className?: string
+}
+
+export function Tree<T>(props: TreeProps<T>) {
+  const { className = "", ...rest } = props
+
+  return (
+    <div class={className}>
+      <NodeList {...rest} />
+    </div>
+  )
+}
