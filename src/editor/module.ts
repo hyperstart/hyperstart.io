8e37110dc74b92c2b9b4cc12065159a72e243eaf
Anthony Ferrando <ferrando.tony@gmail.com>
2018-03-09 12:12:35 +0100

    Start work on editor module

--- src/editor/module.ts
+++ src/editor/module.ts
@@ -1,8 +1,23 @@
 import { ModuleImpl } from "lib/modules"
 
+import { createProjects } from "projects/module"
+
+import { debug } from "./debug/module"
+import { sources } from "./sources/module"
+import { ui } from "./ui/module"
 import * as api from "./api"
 
+// TODO
+const localStore = createProjects(null)
+
 export const editor: ModuleImpl<api.State, api.Actions> = {
-  state: null,
+  state: {
+    debug: debug.state,
+    files: { byId: {}, byPath: {}, roots: [] },
+    localStore: localStore.state,
+    status: "closed",
+    sources: sources.state,
+    ui: ui.state
+  },
   actions: null
 }
