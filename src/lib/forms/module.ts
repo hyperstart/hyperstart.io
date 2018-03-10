405e1d3c31a5bfa2334b86cb5113e91ac9400f7e
Anthony Ferrando <ferrando.tony@gmail.com>
2018-03-10 10:46:17 +0100

    Implement lib/forms module

--- src/lib/forms/module.ts
+++ src/lib/forms/module.ts
@@ -2,11 +2,36 @@ import { ModuleImpl } from "lib/modules"
 
 import * as api from "./api"
 
-// TODO
-
 export function createForms(): ModuleImpl<api.State, api.Actions> {
   return {
-    state: null,
-    actions: null
+    state: {},
+    actions: {
+      setForm: (payload: api.FormUpdate) => state => {
+        const { form, ...values } = payload
+        return {
+          [form]: {
+            ...state[form],
+            ...values
+          }
+        }
+      },
+      setField: (payload: api.FormFieldUpdate) => state => {
+        const { form, field, ...values } = payload
+        const prev = state[form] || { loading: false, fields: {} }
+        const prevField = prev.fields[field]
+        return {
+          [form]: {
+            loading: prev.loading,
+            fields: {
+              ...prev.fields,
+              [field]: {
+                ...prevField,
+                ...values
+              }
+            }
+          }
+        }
+      }
+    }
   }
 }
