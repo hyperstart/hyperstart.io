18824ee09b03d3a0c812359dbc5ff9d3c3b05d92
Anthony Ferrando <ferrando.tony@gmail.com>
2018-03-17 12:14:33 +0100

    Add modal component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export * from "./Body"
 export * from "./Header"
+export * from "./Footer"
---
+++ src/lib/components/Modal.tsx
@@ -0,0 +1,43 @@
+import { h } from "hyperapp"
+
+export interface ModalProps {
+  active: boolean
+  close(): void
+  size?: "sm" | "lg"
+  title?: string
+  titleTag?: string
+  Footer?: any
+}
+
+export function Modal(props: ModalProps, children) {
+  const { active, close, title = "Modal", size } = props
+  const Title = props.titleTag
+  const Footer = props.Footer ? (
+    <div class="modal-footer">
+      <props.Footer />
+    </div>
+  ) : null
+  const className = `modal ${active ? "active" : ""} ${
+    size ? "modal-" + size : ""
+  }`
+  return (
+    <div class={className}>
+      <a href="#" class="modal-overlay" aria-label="Close" onclick={close} />
+      <div class="modal-container">
+        <div class="modal-header">
+          <a
+            href="#"
+            class="btn btn-clear float-right"
+            aria-label="Close"
+            onclick={close}
+          />
+          <Title class="modal-title">{title}</Title>
+        </div>
+        <div class="modal-body">
+          <div class="content">{children}</div>
+        </div>
+        {Footer}
+      </div>
+    </div>
+  )
+}
