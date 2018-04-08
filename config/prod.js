b8ad4c7ba23b21b91fd3055c7316a94272602e31
Anthony Ferrando <ferrando.tony@gmail.com>
2018-04-08 10:56:45 +0200

    Add a local config with local database

--- config/prod.js
+++ config/prod.js
@@ -12,7 +12,6 @@ module.exports = {
   }),
   STORE_CONFIG: JSON.stringify({
     type: "firestore"
-    // type: "local",
   }),
   // the ID of the hyperapp project
   HYPERAPP_ID: JSON.stringify("NYAw2ak7y3R75uHzHWydyk5cViH3-nt_aSLzPVvYX")
